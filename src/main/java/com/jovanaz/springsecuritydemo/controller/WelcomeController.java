46f3d46fb4e3ba6ac1a5e9c75a46e54c4fd05d6e
jovanazivkovic99 <jovanakipar@gmail.com>
2023-04-29 17:00:22 +0200

    Initial commit

---
+++ src/main/java/com/jovanaz/springsecuritydemo/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.jovanaz.springsecuritydemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "Welcome from Spring Application withou Spring Security.";
+    }
+}
