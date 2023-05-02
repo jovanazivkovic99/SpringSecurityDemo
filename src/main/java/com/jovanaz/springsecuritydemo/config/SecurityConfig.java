d691c4058dcd9e48e5a85dff8e9e29df87199bde
jovanazivkovic99 <jovanakipar@gmail.com>
2023-05-02 15:46:10 +0200

    Implemented In-Memory Authentication

--- src/main/java/com/jovanaz/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/com/jovanaz/springsecuritydemo/config/SecurityConfig.java
@@ -1,31 +1,81 @@
 package com.jovanaz.springsecuritydemo.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    /*
-    * /myAccount - Secured
-    * /myBalance - Secured
-    * /myLoans - Secured
-    * /myCards - Secured
-    * /notices- NotSecured
-    * /contact - Not Secured
-    */
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        PasswordEncoder passwordEncoder = passwordEncoder();
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user").password(passwordEncoder.encode("12345")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder.encode("12345")).roles("ADMIN").build()
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain customFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().authenticated()
+                        .requestMatchers("/myAccount").authenticated()
+                        .requestMatchers("/myBalance").authenticated()
+                        .requestMatchers("/myLoans").authenticated()
+                        .requestMatchers("/myCards").authenticated()
+                        .requestMatchers("/notices").permitAll()
+                        .requestMatchers("/contact").permitAll()
                 )
-                .formLogin().and()
+                .formLogin()
+                .and()
                 .httpBasic();
         return http.build();
     }
+
+
+   /* @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                               .username("user")
+                               .password("12345")
+                               .authorities("READ")
+                               .build();
+        UserDetails admin = User.builder()
+                                .username("admin")
+                                .password("12345")
+                                .authorities("ADMIN")
+                                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }*/
+
+
+
+   /* @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//authProvider.wi
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoderCustom());
+
+        return authProvider;
+    }*/
+
 }
